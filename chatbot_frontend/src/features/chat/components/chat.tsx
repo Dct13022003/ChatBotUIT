7d9ec8ee5c2273789f78efc6ff4ab47cb357366a
Tai130203 <nanh02465@gmail.com>
2025-07-05 11:32:07 +0700

    update UI

--- chatbot_frontend/src/features/chat/components/chat.tsx
+++ chatbot_frontend/src/features/chat/components/chat.tsx
@@ -96,7 +96,7 @@ export const Chat = () => {
           <ChatBubbleBottomCenterTextIcon className="w-10 h-10" />
           <h1 className="text-2xl font-bold" style={{ fontSize: "2rem" }}>
             {language === "Tiếng Việt"
-              ? "Chatbot Du lịch Việt Nam"
+              ? "Chatbot học vụ UIT"
               : "Vietnam Travel Chatbot"}
           </h1>
         </div>
